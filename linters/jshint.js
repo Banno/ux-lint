113870fe0427419c32c012b88191fdeb2e070494
Eric Heikes <eric.heikes@banno.com>
2015-12-28 13:36:38 -0600

    Include the filename in any exceptions thrown by the jshint plugin.

--- linters/jshint.js
+++ linters/jshint.js
@@ -13,20 +13,25 @@ exports.check = function(filePattern, opts) {
 	var predefs = opts.globals || {};
 	return readFiles(filePattern).then(function(files) {
 		return files.map(function(fileInfo) {
-			jshint(fileInfo.contents, opts, predefs);
-			return jshint.errors.map(function(error) {
-				error.id = error.id || '(error)'; // "Too many errors" does not have an ID
-				return {
-					character: error.character,
-					code: error.code,
-					description: error.reason,
-					evidence: error.evidence,
-					file: fileInfo.file,
-					line: error.line,
-					plugin: 'jshint',
-					type: error.id.replace(/(^\()|(\)$)/g, ''), // remove the enclosing parens
-				};
-			});
+			try {
+				jshint(fileInfo.contents, opts, predefs);
+				return jshint.errors.map(function(error) {
+					error.id = error.id || '(error)'; // "Too many errors" does not have an ID
+					return {
+						character: error.character,
+						code: error.code,
+						description: error.reason,
+						evidence: error.evidence,
+						file: fileInfo.file,
+						line: error.line,
+						plugin: 'jshint',
+						type: error.id.replace(/(^\()|(\)$)/g, ''), // remove the enclosing parens
+					};
+				});
+			} catch (err) {
+				err.message = err.message + ' (' + fileInfo.file + ')';
+				throw err;
+			}
 		});
 	}).then(function(results) {
 		return flatten(results);
