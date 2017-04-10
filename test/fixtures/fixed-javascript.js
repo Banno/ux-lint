f1da2acf71367b25c30edca59c66c3f12248dfb7
Eric Heikes <eric.heikes@banno.com>
2017-04-10 15:46:31 -0500

    Update test fixture for whitespace change (#28).

--- test/fixtures/fixed-javascript.js
+++ test/fixtures/fixed-javascript.js
@@ -1,19 +1,19 @@
 'use strict';
 (() => {
-	onboarding.controller('createUserController', ['$scope', '$stateParams', '$state',
-		'usersService', function($scope, $stateParams, $state, usersService) {
-			for (let param of $stateParams) {
-				param.foo = 'foo';
-			}
+  onboarding.controller('createUserController', ['$scope', '$stateParams', '$state',
+    'usersService', function($scope, $stateParams, $state, usersService) {
+      for (let param of $stateParams) {
+        param.foo = 'foo';
+      }
 
-			$scope.createUser = function(user) {
-				usersService.createUser(user).then((result) => {
-					$scope.userCreated = true;
-					$state.go('userProfile', { id: result._id });
-				}, () => {
-					$scope.userCreated = false;
-				});
+      $scope.createUser = function(user) {
+        usersService.createUser(user).then((result) => {
+          $scope.userCreated = true;
+          $state.go('userProfile', { id: result._id });
+        }, () => {
+          $scope.userCreated = false;
+        });
 
-			};
-		}]);
+      };
+    }]);
 })();
